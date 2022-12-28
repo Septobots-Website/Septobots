ae7de63832b9f7ce45cb72940e85c5ec5df74c64
Dr-Huitzil <gotenkscoolness300@gmail.com>
2022-12-28 03:14:08 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Navbar.js 	modified:   src/styles/global.css 	modified:   static/favicon.ico

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,19 @@ import Logo from "../components/img/logo.png"
 export default function Navbar() {
   return (
     <section className="navigation">
-    <header>
-        <span className='image-clickable'>
-            <Link to="/">
-                <img src={Logo} alt="logo:" />
-            </Link>
-        </span>
-    <nav>
-        <ul class="nav-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/sponsors">Sponsors</Link></li>
-            <li><Link to="#contactUs">Contact</Link></li>
-        </ul>
-    </nav>
-      <Link to="#"><button>Donate</button></Link>
-</header>
+        <nav>
+         <Link to="/"><img src={Logo} alt="logo" className='image-clickable'/></Link>
+            <div class="nav-links">
+                <ul>    
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/sponsors">Sponsors</Link></li>
+                    <li><Link to="#contactUs">Contact</Link></li>
+                    <li><Link to="#"><button className='hero-btn'>Donate</button></Link></li>
+                </ul>
+            </div>
+        </nav>
     </section>
     
   )
