a91c1025f63f774287d87bbff994e4bea649f4b0
Dr-Huitzil <gotenkscoolness300@gmail.com>
2022-12-27 21:22:21 -0800

    modified:   src/components/MainInfo.js 	modified:   src/styles/global.css

--- src/components/MainInfo.js
+++ src/components/MainInfo.js
@@ -4,10 +4,8 @@ export default function MainInfo() {
   return (
     <section className="mainInfo">
     <div className="paws"></div>
-      <div className="mainInfoText">
           <h1>The Septobots</h1>
-          <h4>First Robotics Competition Team 7777</h4>  
-      </div>    
+          <p>First Robotics Competition Team 7777</p>   
     <div className="paws"></div>
     </section>
   )
