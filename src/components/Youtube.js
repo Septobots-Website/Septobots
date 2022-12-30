2c8d13211407860b3ef4d1359d87cf2ae9d5ba0d
Dr-Huitzil <gotenkscoolness300@gmail.com>
2022-12-29 18:41:56 -0800

    modified:   src/components/Youtube.js 	modified:   src/components/img/video.js 	modified:   src/styles/global.css

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -4,9 +4,11 @@ import YoutubeVideo from "../components/img/video.js"
 export default function Youtube() {
   return (
     <section className="youtubeSection">
-    <div className="videoContainer">
-        <YoutubeVideo/>
-    </div>
+        <h2>2022 Rewind</h2>
+        <p>Join Team 7777 as they reflect on their exciting and successful year in robotics.
+        Don't miss this exciting 2022 rewind video that captures all the memorable moments of the team's journey.
+        </p>
+        <YoutubeVideo/> 
     </section>
   )
 }
